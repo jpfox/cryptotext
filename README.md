39353d4a002f50d67aa751431a98006615033786
Jean-Pierre Morfin <dev@jpfox.fr>
2015-01-27 16:05:46 +0100

    First release : form to test crypto scripts

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
-    $ cordova create cryptotext fr.jpfox.cryptotext CryptoText
-    $ cd cryptotext/
-    $ cordova platform add android
-    $ cordova plugin add org.apache.cordova.contacts
-    $ git init
-    $ git remote add origin http://github/jpfox/cryptotext
-    $ git fetch
-    $ git checkout -t origin/master
-    $ cordova build
+# How to build
+
+## Install Cordova
+
+Follow instructions on [Cordova documentation site](http://cordova.apache.org/docs/en/edge/guide_cli_index.md.html)
+
+## Initialise project
+
+### Create Cordova structure
+
+     $ cordova create cryptotext fr.jpfox.cryptotext CryptoText
+     $ cd cryptotext/
+     $ cordova platform add android
+     $ cordova plugin add org.apache.cordova.contacts
+
+### Get source from github repository into this existing structure
+
+     $ git init
+     $ git remote add origin http://github/jpfox/cryptotext
+     $ git fetch
+     $ git checkout -t origin/master
+
+## Build application
+
+     $ cordova build
+
+## Test in emulator
+
+     $ cordova emulate
 
--- www/js/index.js
+++ www/js/index.js
@@ -37,15 +37,101 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        var devicereadyElement = document.getElementById(id);
+        var viewcryptElement = document.getElementById('viewcrypt');
 
         console.log('Received Event: ' + id);
+        
+        devicereadyElement.setAttribute('style', 'display:none;');
+        viewcryptElement.setAttribute('style', 'display:block;');
     }
 };
 
 app.initialize();
+
+var bits = 512;
+var myRSAKey = null;
+
+$( document ).ready(function() {
+	
+	// disable crypt action on pass change
+	$('#mypass').change(function(){
+		$('.cryptaction').prop( "disabled", true );
+		$('#mypubkey').val('');
+	});
+	
+	// generate key
+	$('#validpass').click(function(){
+		var name = $('#myname').val();
+		var pass = $('#mypass').val();
+		if(pass.length<=5)
+		{
+			alert("Pass phrase must have more than 5 characters");
+			pass = null;
+			return;
+		}
+		if(name.length==0)
+		{
+			alert("You have to enter your name");
+			pass = null;
+			return;
+		}
+		myRSAKey = cryptico.generateRSAKey(name + '/' + pass, bits);
+		$('#mypubkey').val(cryptico.publicKeyString(myRSAKey));
+		$('.cryptaction').prop( "disabled", false );
+		pass = null;
+	});
+	
+	// select on focus
+	$("textarea").mouseup(function(e){
+		// fixes safari/chrome problem
+		e.preventDefault();
+	})
+	.focus(function(e){
+		$(this).select();
+	})
+	.click(function(e){
+		$(this).select();
+	});
+	
+	// encrypt
+	$('#encrypt').click(function(){
+		var sign = $('#sign').prop("checked");
+		try {
+			var encryptionResult = cryptico.encrypt($('#cleartext').val(), $('#contactkey').val(), sign?myRSAKey:null);
+			$('#cryptedtext').val(encryptionResult.cipher);
+		} catch(e) {
+			$('#cryptedtext').val('');
+			$('#signature').html(e.message);
+		}
+	});
+	
+	// decrypt
+	$('#decrypt').click(function(){
+		try {
+			var decryptionResult = cryptico.decrypt($('#cryptedtext').val(), myRSAKey);
+			$('#cleartext').val(decryptionResult.plaintext);
+			if(decryptionResult.status=="success") {
+				if(decryptionResult.signature=='verified')
+				{
+					var signature = decryptionResult.publicKeyString;
+					if($('#contactkey').val()=='')
+						$('#contactkey').val(signature);
+					if(signature==$('#contactkey').val())
+						$('#signature').text("signed by contact.");
+					else
+						$('#signature').text(decryptionResult.publicKeyString);
+				}
+				else
+					$('#signature').text(decryptionResult.signature + " !");
+			} else {
+				$('#signature').text("decryption failed !");
+			}
+		} catch(e) {
+			$('#cleartext').val('');
+			$('#signature').html(e.message);
+		}
+
+	});
+	
+});
